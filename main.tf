96e0b97e3570e152c13a1f3169d5dffbdd813cae
Gabriel Akinmoyero <akinmoyerogabriel@gmail.com>
2024-09-29 00:06:34 +0100

    Update clusterIssuer, ingress and irsa module; Add nginx lb controller

--- main.tf
+++ main.tf
@@ -5,45 +5,85 @@ locals {
 }
 
 
-######## All IAM Policies can be modified to make them more secure ########
-#### ALB Controller Resources ####
+######## Remember: All IAM policies can be modified to enhance their security. ########
+# #### ALB Controller Resources ####
 
-module "alb_controller" {
+# module "alb_controller" {
+#   source                      = "./modules/irsa"
+#   name                        = "alb_controller"
+#   policy_file_path            = file("template/alb-iam-policy.json")
+#   eks_oidc_provider_arn       = module.eks.oidc_provider_arn
+#   eks_oidc_provider           = module.eks.oidc_provider
+#   namespace                   = "kube-system"
+#   service_account_name        = "aws-load-balancer-controller"
+#   create_kube_service_account = true
+
+# }
+
+
+# resource "helm_release" "alb_controller" {
+#   name       = "alb-load-balancer-controller"
+#   verify     = false
+#   repository = "https://aws.github.io/eks-charts"
+#   namespace  = "kube-system"
+#   chart      = "aws-load-balancer-controller"
+#   version    = "1.8.3"
+
+#   set {
+#     name  = "clusterName"
+#     value = module.eks.cluster_name
+#   }
+
+#   set {
+#     name  = "serviceAccount.create"
+#     value = "false"
+#   }
+
+#   set {
+#     name  = "serviceAccount.name"
+#     value = "aws-load-balancer-controller"
+#   }
+#   depends_on = [module.alb_controller]
+# }
+
+#### Nginx Load Balancer Controller Resources ####
+
+module "nginx_controller" {
   source                      = "./modules/irsa"
-  name                        = "alb_controller"
-  policy_file_path            = file("template/alb-iam-policy.json")
+  name                        = "nginx_load_balancer_controller"
+  policy_file_path            = file("template/load-balancer-controller-iam-policy.json")
   eks_oidc_provider_arn       = module.eks.oidc_provider_arn
   eks_oidc_provider           = module.eks.oidc_provider
-  namespace                   = "kube-system"
-  service_account_name        = "aws-load-balancer-controller"
+  namespace                   = "ingress-nginx"
+  service_account_name        = "nginx-controller"
   create_kube_service_account = true
-
 }
 
 
-resource "helm_release" "alb_controller" {
-  name       = "alb-load-balancer-controller"
-  verify     = false
-  repository = "https://aws.github.io/eks-charts"
-  namespace  = "kube-system"
-  chart      = "aws-load-balancer-controller"
-  version    = "1.8.3"
-
-  set {
-    name  = "clusterName"
-    value = module.eks.cluster_name
-  }
+resource "helm_release" "nginx_controller" {
+  name             = "ingress-nginx"
+  repository       = "https://kubernetes.github.io/ingress-nginx"
+  namespace        = "ingress-nginx"
+  chart            = "ingress-nginx"
+  version          = "4.11.2"
+  create_namespace = true
 
   set {
     name  = "serviceAccount.create"
-    value = "false"
+    value = "true"
   }
 
   set {
     name  = "serviceAccount.name"
-    value = "aws-load-balancer-controller"
+    value = module.nginx_controller.service_account_name
   }
-  depends_on = [module.alb_controller]
+
+  set {
+    name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
+    value = module.nginx_controller.role_arn
+  }
+
+  depends_on = [module.nginx_controller]
 }
 
 
@@ -61,21 +101,21 @@ module "cert_manager" {
 }
 
 resource "helm_release" "cert_manager" {
-  name       = "cert-manager"
-  verify     = false
-  repository = "https://charts.jetstack.io"
-  namespace  = "cert-manager"
-  chart      = "cert-manager"
-  version    = "1.15.3"
+  name             = "cert-manager"
+  verify           = false
+  repository       = "https://charts.jetstack.io"
+  namespace        = "cert-manager"
+  chart            = "cert-manager"
+  version          = "1.15.3"
   create_namespace = true
 
   set {
     name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
-    value = "${module.cert_manager.role_arn}"
+    value = module.cert_manager.role_arn
   }
 
   set {
-    name  = "rds.enabled"
+    name  = "crds.enabled"
     value = "true"
   }
 
--- modules/irsa/output.tf
+++ modules/irsa/output.tf
@@ -1,3 +1,7 @@
 output "role_arn" {
     value = aws_iam_role.role.arn
 }
+
+output "service_account_name" {
+    value = var.service_account_name
+}
